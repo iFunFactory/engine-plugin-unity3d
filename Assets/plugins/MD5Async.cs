d8d25b8e9979302335b0321d2593d0e3ddefa2a3
Lee.Eunhee <arin@ifunfactory.com>
2014-08-07 17:02:43 +0900

    md5 계산 비동기 처리
    - 다운받은 파일의 md5 계산을 비동기로 처리
    
    Change-Id: Iaa3e15c691c085d6b7192f4b9d09d3e3da3a283f

---
+++ Assets/plugins/MD5Async.cs
@@ -0,0 +1,77 @@
+﻿// Copyright (C) 2013 iFunFactory Inc. All Rights Reserved.
+//
+// This work is confidential and proprietary to iFunFactory Inc. and
+// must not be used, disclosed, copied, or distributed without the prior
+// consent of iFunFactory Inc.
+
+using System;
+using System.IO;
+using System.Security.Cryptography;
+using UnityEngine;
+
+
+public class MD5Async
+{
+    public static void Compute (string path, OnFinish on_finish)
+    {
+        if (!File.Exists(path))
+        {
+            Debug.Log("MD5Async.Compute - Can't find a file. path: " + path);
+            return;
+        }
+
+        FileStream stream = File.OpenRead(path);
+
+        MD5Request request = new MD5Request();
+        request.md5 = MD5.Create();
+        request.stream = stream;
+        request.on_finish = on_finish;
+
+        int length = (stream.Length < kBlockSize) ? (int)stream.Length : kBlockSize;
+        stream.BeginRead(request.buffer, 0, length, new AsyncCallback(ReadCb), request);
+    }
+
+    static void ReadCb (IAsyncResult ar)
+    {
+        MD5Request request = (MD5Request)ar.AsyncState;
+        FileStream stream = request.stream;
+
+        int length = stream.EndRead(ar);
+
+        if (stream.Position >= stream.Length)
+        {
+            request.md5.TransformFinalBlock(request.buffer, 0, length);
+
+            string md5hash = "";
+            foreach (byte n in request.md5.Hash)
+                md5hash += n.ToString("x2");
+
+            Debug.Log("MD5 >> " + stream.Name + " > " + md5hash);
+
+            if (request.on_finish != null)
+                request.on_finish(md5hash);
+        }
+        else
+        {
+            request.md5.TransformBlock(request.buffer, 0, length, request.buffer, 0);
+
+            length = (stream.Position + kBlockSize > stream.Length) ? (int)(stream.Length - stream.Position) : kBlockSize;
+            stream.BeginRead(request.buffer, 0, length, new AsyncCallback(ReadCb), request);
+        }
+    }
+
+
+    // Buffer-related constants.
+    private static readonly int kBlockSize = 65536;
+
+    // Event handler delegate
+    public delegate void OnFinish (string md5hash);
+
+    class MD5Request
+    {
+        public MD5 md5 = null;
+        public FileStream stream = null;
+        public byte[] buffer = new byte[kBlockSize];
+        public OnFinish on_finish = null;
+    }
+}
