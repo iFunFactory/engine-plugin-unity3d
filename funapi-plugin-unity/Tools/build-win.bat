1f27b42a8e374b4a3f13030d2efbf83ab8ca03ac
Jinuk Kim <jinuk.kim@ifunfactory.com>
2015-03-10 13:14:15 +0900

    Windows/linux protobuf dll 빌드스크립트 수정
    - protoc.exe (Windows 용 실행 파일) 추가
    - .cs 생성할 때 .proto -> .bin -> .cs 를 하게 수정
    
    Change-Id: I4a82cbe2cbd058463bd58cca165b7f47a8628b0e

--- funapi-plugin-unity/Tools/build-win.bat
+++ funapi-plugin-unity/Tools/build-win.bat
@@ -10,17 +10,19 @@ set UNITY_MONO=C:\Program Files (x86)\Unity\Editor\Data
 
 set PATH=%PATH%;"%UNITY_MONO%\Mono\bin"
 
+REM To generate .cs file, remove REM in following 3 lines.
 REM echo Generating C# protocol files
-REM protobuf-net\ProtoGen\protogen -i:proto-files\funapi\network\fun_message.proto -o:csharp-files\fun_message.cs -p:detectMissing
-REM protobuf-net\ProtoGen\protogen -i:proto-files\pbuf_echo.proto -o:csharp-files\pbuf_echo.cs -p:detectMissing
+REM protobuf-net\ProtoGen\protoc.exe -I proto-files --include_imports -I proto-files -o messages.bin proto-files\funapi-network\fun_message.proto proto-files\pbuf-echo.proto
+REM protobuf-net\ProtoGen\protogen.exe -i:messages.bin -o:messages.cs -p:detectMissing
 
 echo Generating Protocol DLL
 call gmcs -target:library -unsafe+ ^
     -out:%OUTPUT_ROOT%\DLL\messages.dll ^
     /r:%OUTPUT_ROOT%\DLL\protobuf-net.dll ^
-    csharp-files\*.cs
+    messages.cs
 
 echo Generating serializer DLL
 protobuf-net\Precompile\precompile %OUTPUT_ROOT%\messages.dll ^
+    -probe:%OUTPUT_ROOT%\DLL\ ^
     -o:%OUTPUT_ROOT%\DLL\FunMessageSerializer.dll ^
     -t:FunMessageSerializer
