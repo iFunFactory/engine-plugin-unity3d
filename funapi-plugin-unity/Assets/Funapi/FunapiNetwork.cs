e573a0cb446137085245deb37cf2536c320603f4
Lee Eunhee <arin@ifunfactory.com>
2015-09-21 18:32:07 +0900

    공지사항 항목에 이미지 url 이 없을 때도 다운로드를 시도하는 버그 수정
    - url 항목이 비어있을 경우 다운로드 시도하지 않도록 수정
    - 공지사항 관련 파일 다운로드 폴더명을 announce 로 변경
    
    Change-Id: Ie388e48cc7f9281eddc764542cc5c02960af0155

--- funapi-plugin-unity/Assets/Funapi/FunapiNetwork.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiNetwork.cs
@@ -27,7 +27,7 @@ namespace Fun
     internal class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 104;
+        public static readonly int kPluginVersion = 105;
     }
 
     // Sending message-related class.
