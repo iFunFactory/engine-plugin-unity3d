b2a2403e5d814b032a5bd10de091248ea8f66aaf
JunHyun Park <junhyunpark@ifunfactory.com>
2015-11-27 10:40:32 +0900

    맥 실행파일 창모드에서 배경색 초기화가 안 되어서 깜빡이는 문제 수정
    (FunapiTester scene 에 camera 추가)
    
    Change-Id: I8d635958c8ccc0e62980739097dd89b378b7820c

--- funapi-plugin-unity/Assets/Funapi/FunapiNetwork.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiNetwork.cs
@@ -27,7 +27,7 @@ namespace Fun
     internal class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 118;
+        public static readonly int kPluginVersion = 119;
     }
 
     // Sending message-related class.
