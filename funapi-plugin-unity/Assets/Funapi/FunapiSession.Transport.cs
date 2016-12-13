bb1040460b6df1a75daddc6537d5787e17974855
Lee Eunhee <arin@ifunfactory.com>
2016-12-13 09:16:48 +0900

    TransportOption의 Https 옵션이름 변경
    Https -> HTTPS로 변경
    
    Change-Id: Iccf343a8a40b400189d4889ffb6390b4e8a24f54

--- funapi-plugin-unity/Assets/Funapi/FunapiSession.Transport.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiSession.Transport.cs
@@ -79,7 +79,7 @@ namespace Fun
 
     public class HttpTransportOption : TransportOption
     {
-        public bool Https = false;
+        public bool HTTPS = false;
         public bool UseWWW = false;
     }
 
