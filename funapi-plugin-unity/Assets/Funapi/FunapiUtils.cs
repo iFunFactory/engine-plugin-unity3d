d82520eabdfd6b68a58adf3a792132e0e981208e
Lee Eunhee <arin@ifunfactory.com>
2017-05-08 13:58:31 +0900

    암호화 Public Key 를 에디터에서 수정할 수 있도록 변경
    - Scene의 Tester 오브젝트에 encryptionPublicKey 항목 추가
    
    Change-Id: I48eef2d4ed7dc744cec4eadb59c90de5f7496abe

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -19,7 +19,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 202;
+        public static readonly int kPluginVersion = 207;
     }
 
 
