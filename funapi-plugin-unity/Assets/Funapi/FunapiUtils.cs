685f7ff733b01d854fa7d965a7b5014ad54c2a60
Lee Eunhee <arin@ifunfactory.com>
2017-09-12 15:21:19 +0900

    Transport 연결 / 종료 처리를 큐를 이용해서 Update 시점에 하도록 수정
    Change-Id: I87a9ea0d52f8a1d2d64f126006dbc69faf3a95f7

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -19,7 +19,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 230;
+        public static readonly int kPluginVersion = 231;
     }
 
 
