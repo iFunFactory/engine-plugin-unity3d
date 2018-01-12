8ff1cb449b943480e819ac7562635da2834f3a26
Lee Eunhee <arin@ifunfactory.com>
2018-01-12 14:10:15 +0900

    DummyEncryption 타입의 메시지를 처리하지 못하는 버그 수정
    Change-Id: I8f70c13faba7873198cdb881424cd50d45054c16

--- funapi-plugin-unity/Assets/Funapi/FunapiEncryption.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiEncryption.cs
@@ -581,6 +581,9 @@ namespace Fun
 
         protected bool encryptMessage (FunapiMessage message, EncryptionType type, ref string header)
         {
+            if (type == EncryptionType.kDummyEncryption)
+                return true;
+
             if (!encryptors_.ContainsKey(type))
             {
                 LogWarning("Encryptor.encryptMessage - Unavailable type: {0}", type);
@@ -614,6 +617,9 @@ namespace Fun
         protected bool decryptMessage (ArraySegment<byte> buffer, string encryption_type, string encryption_header)
         {
             EncryptionType type = (EncryptionType)Convert.ToInt32(encryption_type);
+            if (type == EncryptionType.kDummyEncryption)
+                return true;
+
             if (!encryptors_.ContainsKey(type))
             {
                 LogWarning("Encryptor.decryptMessage - Unavailable type: {0}", type);
