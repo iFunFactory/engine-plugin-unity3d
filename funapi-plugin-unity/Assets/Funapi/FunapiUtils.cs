cf9b911a8d0d553cb7636377496e77cd63ffdb87
Lee Eunhee <arin@ifunfactory.com>
2018-02-19 10:02:27 +0900

    Deflate 테스트 추가
    Change-Id: Ida0d2e65b1fe5f5d9d4ec96b1e8978409d5ec247

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -19,7 +19,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 244;
+        public static readonly int kPluginVersion = 245;
     }
 
 
