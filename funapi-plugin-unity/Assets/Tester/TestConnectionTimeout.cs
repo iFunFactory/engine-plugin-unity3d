7e16349b24c7b0ddd93747ef4eaee090163d1dd7
Lee Eunhee <arin@ifunfactory.com>
2018-03-06 11:43:03 +0900

    Transport에서 사용하던 타이머를 FunapiTimer로 변경
    - Delayed Ack, Connection Timeout, Ping 에 적용
    
    - TimeoutTimer에 Loop 옵션 추가
    
    - Connection Timeout 테스트 추가
    
    Change-Id: I7d963a64d921be061837762652a5fde10a9879a6

---
+++ funapi-plugin-unity/Assets/Tester/TestConnectionTimeout.cs
@@ -0,0 +1,92 @@
+﻿// Copyright 2018 iFunFactory Inc. All Rights Reserved.
+//
+// This work is confidential and proprietary to iFunFactory Inc. and
+// must not be used, disclosed, copied, or distributed without the prior
+// consent of iFunFactory Inc.
+
+using Fun;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+
+public class TestConnectionTimeout
+{
+    [UnityTest]
+    public IEnumerator TCP_Protobuf ()
+    {
+        yield return new TestImpl (TransportProtocol.kTcp, FunEncoding.kProtobuf);
+    }
+
+    [UnityTest]
+    public IEnumerator HTTP_Json ()
+    {
+        yield return new TestImpl (TransportProtocol.kHttp, FunEncoding.kJson);
+    }
+
+
+    class TestImpl : TestSessionBase
+    {
+        public TestImpl (TransportProtocol protocol, FunEncoding encoding)
+        {
+            TransportOption option = newTransportOption(protocol);
+            option.ConnectionTimeout = 0.01f;
+
+            session = FunapiSession.Create(TestInfo.ServerIp);
+
+            session.SessionEventCallback += delegate (SessionEventType type, string sessionid)
+            {
+                if (type == SessionEventType.kStopped)
+                {
+                    if (test_step > kStepCountMax)
+                    {
+                        FunapiSession.Destroy(session);
+                        isFinished = true;
+                    }
+                }
+            };
+
+            session.TransportEventCallback += delegate (TransportProtocol p, TransportEventType type)
+            {
+                if (isFinished)
+                    return;
+
+                if (type == TransportEventType.kStarted)
+                {
+                    sendEchoMessage(protocol);
+                }
+                else if (type == TransportEventType.kStopped)
+                {
+                    ++test_step;
+                    if (test_step < kStepCountMax)
+                    {
+                        ushort port = getPort("default", protocol, encoding);
+                        session.Connect(protocol, encoding, port, option);
+                    }
+                    else
+                    {
+                        option.ConnectionTimeout = 3f;
+                        ushort port = getPort("default", protocol, encoding);
+                        session.Connect(protocol, encoding, port, option);
+                    }
+                }
+            };
+
+            session.ReceivedMessageCallback += delegate (string type, object message)
+            {
+                onReceivedEchoMessage(type, message);
+
+                if (isReceivedAllMessages)
+                    session.Stop();
+            };
+
+            setTimeoutCallback(3f);
+
+            session.Connect(protocol, encoding, 80, option);
+        }
+
+
+        const int kStepCountMax = 3;
+        int test_step = 0;
+    }
+}
