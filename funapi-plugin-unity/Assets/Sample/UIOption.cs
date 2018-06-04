e076c54118232cab4dbd703b247e4bb8c7bbed09
Eunhee Lee <arin@ifunfactory.com>
2018-06-04 10:34:58 +0900

    샘플 코드 수정
    Change-Id: I1eff14d34f2aef14119085807eb207e7b91ff085

---
+++ funapi-plugin-unity/Assets/Sample/UIOption.cs
@@ -0,0 +1,79 @@
+﻿// Copyright 2013 iFunFactory Inc. All Rights Reserved.
+//
+// This work is confidential and proprietary to iFunFactory Inc. and
+// must not be used, disclosed, copied, or distributed without the prior
+// consent of iFunFactory Inc.
+
+using Fun;
+using UnityEngine;
+
+
+public partial class UIOption : MonoBehaviour
+{
+    void Awake()
+    {
+        fields_ = fieldsObject.GetComponent<UIOptionFields>();
+        fields_.Init();
+
+        data_ = new Data();
+        if (!PlayerPrefs.HasKey("address"))
+            data_.Init();
+
+        loadData();
+    }
+
+    void OnDestroy()
+    {
+        saveData();
+        gameObject.SetActive(false);
+    }
+
+
+    void loadData ()
+    {
+        fields_.address.text = data_.address;
+        fields_.port.text = data_.port.ToString();
+        fields_.protocol.typeInt = data_.protocol;
+        fields_.encoding.typeInt = data_.encoding;
+    }
+
+    void saveData ()
+    {
+        if (!checkChanges())
+            return;
+
+        data_.address = fields_.address.text;
+        data_.port = int.Parse(fields_.port.text);
+        data_.protocol = fields_.protocol.typeInt;
+        data_.encoding = fields_.encoding.typeInt;
+    }
+
+    bool checkChanges ()
+    {
+        if (bChanged)
+            return true;
+
+        if (data_.address != fields_.address.text ||
+            data_.port != int.Parse(fields_.port.text) ||
+            data_.protocol != fields_.protocol.typeInt ||
+            data_.encoding != fields_.encoding.typeInt)
+        {
+            bChanged = true;
+        }
+
+        return bChanged;
+    }
+
+
+    public string address { get { return fields_.address.text; } }
+    public ushort port { get { return ushort.Parse(fields_.port.text); } }
+    public TransportProtocol protocol { get { return fields_.protocol.type; } }
+    public FunEncoding encoding { get { return fields_.encoding.type; } }
+
+    public bool bChanged { get; set; }
+
+    public GameObject fieldsObject;
+
+    UIOptionFields fields_;
+    Data data_;
+}
---
+++ funapi-plugin-unity/Assets/Sample/UIOptionFields.cs
@@ -0,0 +1,154 @@
+﻿// Copyright 2013 iFunFactory Inc. All Rights Reserved.
+//
+// This work is confidential and proprietary to iFunFactory Inc. and
+// must not be used, disclosed, copied, or distributed without the prior
+// consent of iFunFactory Inc.
+
+using Fun;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class UIOptionFields : MonoBehaviour
+{
+    public void Init ()
+    {
+        Dropdown[] dnlist = transform.GetComponentsInChildren<Dropdown>();
+        Dictionary<string, Dropdown> drops = new Dictionary<string, Dropdown>();
+        foreach (Dropdown d in dnlist)
+            drops[d.name] = d;
+
+        protocol = new Protocol(drops["Protocol"]);
+        encoding = new Encoding(drops["Encoding"]);
+    }
+
+
+    public class Protocol
+    {
+        public Protocol (Dropdown dn)
+        {
+            list_ = dn;
+            type_ = getType(dn.value);
+        }
+
+        public TransportProtocol type
+        {
+            get
+            {
+                type_ = getType(list_.value);
+                return type_;
+            }
+            set
+            {
+                type_ = value;
+                list_.value = getType(value);
+            }
+        }
+
+        public int typeInt
+        {
+            get
+            {
+                type_ = getType(list_.value);
+                return list_.value;
+            }
+            set
+            {
+                type_ = getType(value);
+                list_.value = value;
+            }
+        }
+
+        int getType (TransportProtocol value)
+        {
+            if (value == TransportProtocol.kTcp)
+                return 0;
+            else if (value == TransportProtocol.kUdp)
+                return 1;
+            else if (value == TransportProtocol.kHttp)
+                return 2;
+            else if (value == TransportProtocol.kWebsocket)
+                return 3;
+
+            return 0;
+        }
+
+        TransportProtocol getType (int value)
+        {
+            if (value == 0)
+                return TransportProtocol.kTcp;
+            else if (value == 1)
+                return TransportProtocol.kUdp;
+            else if (value == 2)
+                return TransportProtocol.kHttp;
+            else if (value == 3)
+                return TransportProtocol.kWebsocket;
+
+            return TransportProtocol.kDefault;
+        }
+
+        Dropdown list_;
+        TransportProtocol type_;
+    }
+
+
+    public class Encoding
+    {
+        public Encoding (Dropdown dn)
+        {
+            list_ = dn;
+            type_ = getType(dn.value);
+        }
+
+        public FunEncoding type
+        {
+            get
+            {
+                type_ = getType(list_.value);
+                return type_;
+            }
+            set
+            {
+                type_ = value;
+                list_.value = getType(value);
+            }
+        }
+
+        public int typeInt
+        {
+            get
+            {
+                type_ = getType(list_.value);
+                return list_.value;
+            }
+            set
+            {
+                type_ = getType(value);
+                list_.value = value;
+            }
+        }
+
+        int getType (FunEncoding value)
+        {
+            return value == FunEncoding.kJson ? 0 : 1;
+        }
+
+        FunEncoding getType (int value)
+        {
+            if (value == 0)
+                return FunEncoding.kJson;
+            else
+                return FunEncoding.kProtobuf;
+        }
+
+        Dropdown list_;
+        FunEncoding type_;
+    }
+
+
+    public InputField address;
+    public InputField port;
+    public Protocol protocol;
+    public Encoding encoding;
+}
