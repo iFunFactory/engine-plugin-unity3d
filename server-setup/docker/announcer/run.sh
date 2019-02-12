f5f425259275756dac4cca0d24a738740ea0a69f
Eunhee Lee <arin@ifunfactory.com>
2019-02-12 13:34:26 +0900

    단위 테스트 관련 파일 삭제, README 업데이트
    Change-Id: Ia66a0890fe5d7087e94c059248da1ad97d5c3f54

--- server-setup/docker/announcer/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -t --name plugin-announcer -p 8080:8080 plugin:announcer
--- server-setup/docker/compression/run.sh
+++
@@ -1,1 +0,0 @@
-docker run -t --name plugin-compress -p 8071:8071 -p 8072:8072/udp -p 8073:8073 -p 8077:8077 -p 8078:8078/udp -p 8079:8079 plugin:compress
