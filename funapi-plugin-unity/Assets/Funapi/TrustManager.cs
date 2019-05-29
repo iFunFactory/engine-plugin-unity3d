f940ce741ad737d10cbb34c4dd2f6134223f79da
sypark <sypark@ifunfactory.com>
2019-05-29 14:18:47 +0900

    2018.2 이전 버전에서 인증서 다운로드 실패하는 현상 수정
    MozRoots 다운로드 경로 수정 커밋에서 수정된 url로 접속 시 TLS 1.2 가 사용되는데
    2018.2 이후 버전부터 TLS 1.2를 지원하여 2018.2 이전 버전에서는 인증서 다운로드를 실패하는 현상이 있어
    이를 해결하기 위해 인증서 다운로드 시에는 인증이 무조건 성공하도록 인증 콜백 함수를 수정
    
    Change-Id: Ibeb311d7560ae9ed064705781c79029b6eec61cf

--- funapi-plugin-unity/Assets/Funapi/TrustManager.cs
+++ funapi-plugin-unity/Assets/Funapi/TrustManager.cs
@@ -30,6 +30,13 @@ namespace Fun
             string tempPath = FunapiUtils.GetLocalDataPath + "/" + Path.GetRandomFileName();
             try
             {
+                // for pre-2018.2(.NET 4.x) versions
+                ServicePointManager.ServerCertificateValidationCallback = delegate
+                        (System.Object sender, X509Certificate certificate,
+                         X509Chain chain, SslPolicyErrors sslPolicyErrors) {
+                    return true;
+                };
+
                 // Try to download the Mozilla's root certificates file.
                 WebClient webClient = new WebClient();
                 webClient.DownloadFile(kMozillaCertificatesUrl, tempPath);
