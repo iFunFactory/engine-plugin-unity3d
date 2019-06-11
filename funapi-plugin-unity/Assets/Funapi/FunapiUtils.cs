312e64042aaf6d3a424eb3e7b53f3b9381a0c5dd
sypark <sypark@ifunfactory.com>
2019-06-11 15:30:13 +0900

    프로토콜 버퍼 메시지 내용 로그 출력 추가
    프로토콜 버퍼로 통신시 메시지 내용 없이 길이만 출력되었는데
    내용도 출력되도록 수정
    
    Newton Json.Net 사용하기 위해 JSON .NET For Unity Asset 추가
    
    Change-Id: I3acdb3ec8f107fc299a32471e491bdee31b6bc65

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -18,7 +18,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 308;
+        public static readonly int kPluginVersion = 309;
     }
 
 
