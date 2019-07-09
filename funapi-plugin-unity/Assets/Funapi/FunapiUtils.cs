f63d10abf879ee91e9ccbfe9aa7f610bbce73d53
sypark <sypark@ifunfactory.com>
2019-07-09 15:07:47 +0900

    데디케이티드서버 - 버전 정보 보낼 때 동기 방식으로 보내도록 수정
    호스트 매니저가 버전 정보를 확인하기 위해 데디케이티드서버를 실행할 경우
    데디케이티드서버는 호스트 매니저로 버전 정보를 보낸후 종료.
    이 때 데디케이티드 서버가 비동기 방식으로 통신하여 버전 정보가 호스트 매니저로
    모두 전송되지 않은 상태인데도 데디케이티드 서버가 종료되는 현상이 있어,
    버전 정보를 보낼때 동기로 통신하고 응답을 받은 후에 데디케이티드서버를 종료 하도록 수정
    
    Change-Id: I6e094c5fa5c38fd9d8baba5b87f5fcdf5835eed0

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -18,7 +18,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 310;
+        public static readonly int kPluginVersion = 311;
     }
 
 
