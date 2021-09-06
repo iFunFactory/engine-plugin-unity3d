72e29904b0000479919bd8f04cc6c46cc8104e41
Seonyul Park <sypark@ifunfactory.com>
2021-09-06 11:31:57 +0900

    pbuf redirect 시 close reason 추가된 dll 적용
    Change-Id: Ib0c55bc6f51f9d89bae7ea8f87fbd8f84f91a712

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -18,7 +18,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 371;
+        public static readonly int kPluginVersion = 372;
     }
 
 
