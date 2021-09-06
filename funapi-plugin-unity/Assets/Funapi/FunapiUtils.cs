8c802add33515032a85026845be5c01e6a6ce56b
Seonyul Park <sypark@ifunfactory.com>
2021-09-06 09:47:31 +0900

    웹소켓 tls 1.2 적용 시 잘못된 부분 수정
    wss 미사용 시에 ssl configuration 설정 오류 발생하는 부분 수정
    
    Change-Id: I9737ee086e035e6d46fad87d89de1efaddfcae0b

--- funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
+++ funapi-plugin-unity/Assets/Funapi/FunapiUtils.cs
@@ -18,7 +18,7 @@ namespace Fun
     public class FunapiVersion
     {
         public static readonly int kProtocolVersion = 1;
-        public static readonly int kPluginVersion = 370;
+        public static readonly int kPluginVersion = 371;
     }
 
 
